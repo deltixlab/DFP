47e6efc41c70d68a63f9c74a93241e58458d53f5
Barys Chupryn <bchupryn@deltixlab.com>
2020-01-29 09:12:54 +0300

    Java: benchmark for canonize() added

---
+++ java/src/jmh/java/deltix/dfp/CanonizeBenchmark.java
@@ -0,0 +1,36 @@
+package deltix.dfp;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(time = 2, iterations = 4)
+@Measurement(time = 2, iterations = 4)
+@State(Scope.Thread)
+@Fork(3)
+public class CanonizeBenchmark {
+    /* 10., 1000_000., 123.456789123, 1.23, null(=NaN) */
+    @Param({"3584865303386914826", "3584865303387914816", "3503800633551035011", "3566850904877432955", "-128"})
+    private long decimalValue;
+
+    @Benchmark
+    public long canonize() {
+        return Decimal64Utils.canonize(decimalValue);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+            .include(".*" + UnaryOperationBenchmark.class.getSimpleName() + ".*")
+            .forks(1)
+            .build();
+        new Runner(opt).run();
+    }
+}
+
