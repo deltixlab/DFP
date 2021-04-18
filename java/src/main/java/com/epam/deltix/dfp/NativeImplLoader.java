9d2393fbaa06b190f35d9536493ab81a37fa3872
Andrei Davydov <adavydov@deltixlab.com>
2021-04-18 23:52:29 +0300

    Fix loader

--- java/src/main/java/com/epam/deltix/dfp/NativeImplLoader.java
+++ java/src/main/java/com/epam/deltix/dfp/NativeImplLoader.java
@@ -3,22 +3,27 @@ package com.epam.deltix.dfp;
 import rtmath.utilities.ResourceLoader;
 
 import java.nio.file.Paths;
+import java.util.Locale;
 
 public class NativeImplLoader {
     public static void load() {
         try {
-            final String archName = "x" + System.getProperty("sun.arch.data.model");
             String version = NativeImplLoader.class.getPackage().getImplementationVersion();
             if (version == null)
                 version = "0.0.0-SNAPSHOT";
             final boolean isSnapshot = version.endsWith("-SNAPSHOT");
             if (isSnapshot)
                 version = version.substring(0, version.length() - "-SNAPSHOT".length());
+
+            String osName = System.getProperty("os.name");
+            if (osName.toLowerCase(Locale.ROOT).contains("windows"))
+                osName = "Windows";
+
+            final String osArch = System.getProperty("os.arch");
+
             ResourceLoader
-                .from(NativeImpl.class, "$(OS)/$(ARCH)/*") // This version now also works, but is probably less efficient
-                //.from(NativeImplLoader.class, "$(OS)/$(ARCH)/DecimalNative.$(DLLEXT).zst")
-                .to(Paths.get(System.getProperty("java.io.tmpdir"), "com", "EPAM", "Deltix", "DFP", version, archName).toString())
-                // .lowercasePathOnLinux(false)
+                .from(NativeImpl.class, osName + "/" + osArch + "/*") // This version now also works, but is probably less efficient
+                .to(Paths.get(System.getProperty("java.io.tmpdir"), "com", "EPAM", "Deltix", "DFP", version, osArch).toString())
                 .alwaysOverwrite(isSnapshot)
                 .tryRandomFallbackSubDirectory(true)
                 .load();
