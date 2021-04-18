5669a7df572d270bd55e6eb4a5e4221daacab170
Andrei Davydov <adavydov@deltixlab.com>
2021-04-18 18:49:18 +0300

    Update Linux compilation script

--- native/buildLinux.sh
+++ native/buildLinux.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir buildx86_64
-cd buildx86_64
-cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang ../
-make
-mkdir ../bin/Release/Linux/x86_64
-cp ./*.so ./bin/Release/Linux/x86_64
-cd ..
+COMPILER=${3:-clang}
 
+mkdir build$2
+cd build$2
+cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_C_COMPILER=$COMPILER -DCMAKE_CXX_COMPILER=$COMPILER ../
+make
+mkdir -p ../bin/Release/Linux/$2
+cp ./*.so ../bin/Release/Linux/$2
+cd ..
