db1027df94193a6b3fdf056840a72bcdc74ae329
Andrei Davydov <adavydov@deltixlab.com>
2021-04-19 15:58:35 +0300

    Fix version string generation.

--- java/src/main/java/com/epam/deltix/dfp/NativeImplLoader.java
+++ java/src/main/java/com/epam/deltix/dfp/NativeImplLoader.java
@@ -2,15 +2,34 @@ package com.epam.deltix.dfp;
 
 import rtmath.utilities.ResourceLoader;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class NativeImplLoader {
     public static void load() {
         try {
-            String version = NativeImplLoader.class.getPackage().getImplementationVersion();
-            if (version == null)
-                version = "0.0.0-SNAPSHOT";
+            String version = "0.0.0-SNAPSHOT";
+            {
+                final Class<?> clazz = NativeImplLoader.class;
+                final URL clazzResource = clazz.getResource(clazz.getSimpleName() + ".class");
+                if (clazzResource != null) {
+                    final String classPath = clazzResource.toString();
+                    if (classPath.startsWith("jar")) {
+                        final String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) +
+                            "/META-INF/MANIFEST.MF";
+                        try (final InputStream manifestStream = new URL(manifestPath).openStream()) {
+                            final Manifest manifest = new Manifest(manifestStream);
+                            final Attributes attr = manifest.getMainAttributes();
+                            version = attr.getValue("Implementation-Version");
+                        }
+                    }
+                }
+            }
+
             final boolean isSnapshot = version.endsWith("-SNAPSHOT");
             if (isSnapshot)
                 version = version.substring(0, version.length() - "-SNAPSHOT".length());
