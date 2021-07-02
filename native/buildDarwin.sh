bdf1304fc7c0be304882f9beb04d9fd2e73dff11
Andrei Davydov <adavydov@deltixlab.com>
2021-07-02 18:48:45 +0300

    Add initial version of the Darwin build

---
+++ native/buildDarwin.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+CCOMPILER=${2:-clang}
+CXXCOMPILER=${2:-clang++}
+VERBOSE=${3:-ON}
+
+rm -rf ./build
+mkdir build
+cd build
+
+$CCOMPILER --version
+
+cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_INSTALL_PREFIX=../bin/Release/Darwin/amd64 ../
+make install
+
+cd ..
+rm -rf ./build
