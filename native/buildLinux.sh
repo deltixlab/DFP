c9c4e1f4df96bb4e900632df74a0b93e9902677e
Andrei Davydov <adavydov@deltixlab.com>
2021-08-11 22:15:51 +0300

    Fix: linux demo path

--- native/buildLinux.sh
+++ native/buildLinux.sh
@@ -19,20 +19,20 @@ cp ./EXAMPLES/demo ../binDemo/Release/Linux/arm
 rm -rf ./*
 cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DTARGET=aarch64-linux-gnu -DSYSROOT=../../../llvm/clang+llvm-12.0.0-aarch64-linux-gnu -DCMAKE_INSTALL_PREFIX=../bin/Release/Linux/aarch64 ../
 make install
-mkdir -p ../bin/Release/Linux/aarch64
-cp ./EXAMPLES/demo ../bin/Release/Linux/aarch64
+mkdir -p ../binDemo/Release/Linux/aarch64
+cp ./EXAMPLES/demo ../binDemo/Release/Linux/aarch64
 
 rm -rf ./*
 cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DTARGET=i686-linux-gnu -DSYSROOT=../../../llvm/clang+llvm-12.0.0-i386-unknown-freebsd12 -DCMAKE_INSTALL_PREFIX=../bin/Release/Linux/i386 ../
 make install
-mkdir -p ../bin/Release/Linux/i386
-cp ./EXAMPLES/demo ../bin/Release/Linux/i386
+mkdir -p ../binDemo/Release/Linux/i386
+cp ./EXAMPLES/demo ../binDemo/Release/Linux/i386
 
 rm -rf ./*
 cmake -G "Unix Makefiles" -DAPI_PREFIX=$1 -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_INSTALL_PREFIX=../bin/Release/Linux/amd64 ../
 make install
-mkdir -p ../bin/Release/Linux/amd64
-cp ./EXAMPLES/demo ../bin/Release/Linux/amd64
+mkdir -p ../binDemo/Release/Linux/amd64
+cp ./EXAMPLES/demo ../binDemo/Release/Linux/amd64
 
 cd ..
 rm -rf ./build
