a7f61bed95fbc5789c99606b982e42ac3b70cfa2
Andrei Davydov <adavydov@deltixlab.com>
2021-08-12 17:38:01 +0300

    Native: collect Windows demo artifacts.

--- native/buildWindows.bat
+++ native/buildWindows.bat
@@ -4,6 +4,8 @@ cmake -G "Visual Studio 16 2019" -A Win32 -DAPI_PREFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=Win32 /p:Configuration=Release /t:Rebuild ./native.sln
 mkdir ..\bin\Release\Windows\x86
 copy /y Release\*.dll ..\bin\Release\Windows\x86
+mkdir ..\binDemo\Release\Windows\x86
+copy /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\x86
 
 cd ..
 
@@ -13,4 +15,6 @@ cmake -G "Visual Studio 16 2019" -A x64 -DAPI_PREFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=x64 /p:Configuration=Release /t:Rebuild ./native.sln
 mkdir ..\bin\Release\Windows\amd64
 copy /y Release\*.dll ..\bin\Release\Windows\amd64
+mkdir ..\binDemo\Release\Windows\amd64
+copy /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\amd64
 cd ..
